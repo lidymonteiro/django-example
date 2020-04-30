9537fad717e04fe14232e258990c6b8e268d3203
lidymonteiro <contato.lidymonteiro@hotmail.com>
2020-04-30 02:12:32 -0300

    bug fix

--- djangoexample/settings.py
+++ djangoexample/settings.py
@@ -88,7 +88,7 @@ if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'djangoexample_act',
+            'NAME': 'postgres',
             'USER': 'postgres',
             'PASSWORD': 'postgres',
             'HOST': '127.0.0.1',
