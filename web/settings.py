b09c05f1e4f8178f0832d92c91302cdd8ad4a112
lidymonteiro <contato.lidymonteiro@hotmail.com>
2020-05-18 18:01:27 -0300

    microservice web - register csv

--- web/settings.py
+++ web/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
