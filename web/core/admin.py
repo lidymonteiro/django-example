c59cdfabce478970abd403cc0314d31ff29194df
lidymonteiro <contato.lidymonteiro@hotmail.com>
2020-05-18 18:01:27 -0300

    microservice web - register csv

--- web/core/admin.py
+++ web/core/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Extraction, DataExtraction
+import csv
 
-# Register your models here.
+
+def read_data_csv(path):
+    with open(path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        data = []
+        for row in reader:
+            data.append(dict(row))
+        return data
+
+
+# action que converte para JSON. Qual a melhor maneira de salvar no campo JSONField de DataExtraction?
+def spider(modeladmin, request, queryset):
+    for extraction in queryset:
+        csv_file_path = extraction.lawsuits.path
+        dataset = read_data_csv(csv_file_path)
+    print(dataset)
+
+
+spider.short_description = "Apply Spider"
+
+
+class ExtractionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_date', 'total_lawsuits', 'found']
+    actions = [spider]
+
+
+admin.site.register(Extraction, ExtractionAdmin)
+admin.site.register(DataExtraction)
--- web/core/models.py
+++ web/core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+
+class DataExtraction(models.Model):
+    dataset = JSONField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Extraction(models.Model):
+    name = models.CharField(max_length=100, blank=False, default=None)
+    created_date = models.DateTimeField(default=timezone.now)
+    lawsuits = models.FileField(blank=True, null=False)
+    customer = models.CharField(max_length=100, blank=False, default=None)
+    result = models.FileField(blank=True, null=True)
+    total_lawsuits = models.CharField(
+        max_length=100, null=True, blank=True, default=None)
+    found = models.CharField(
+        max_length=100, null=True, blank=True, default=None)
+    status = models.CharField(
+        max_length=100, null=True, blank=True, default=None)
+
+    def __str__(self):
+        return self.name
+
+    def save(self):
+        self.total_lawsuits = sum(1 for line in self.lawsuits.read())
+        super(Extraction, self).save()
